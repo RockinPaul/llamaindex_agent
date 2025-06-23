966933e76bb14ece818d9f1956711f935e61f253
Paul Zarudnev <rockinpaulz@gmail.com>
2025-06-23 21:42:58 +0200

    feat: implement loop event handling in multi-step workflow with random branching

--- workflow.py
+++ workflow.py
@@ -1,5 +1,7 @@
+# https://huggingface.co/learn/agents-course/unit2/llama-index/workflows
 from llama_index.core.workflow import StartEvent, StopEvent, Workflow, Event, step
 import random
+
 
 class MyWorkflow(Workflow):
     @step
@@ -7,15 +9,36 @@ class MyWorkflow(Workflow):
         # do something here
         return StopEvent(result="Hello, world!")
 
+
 class ProcessingEvent(Event):
     intermediate_result: str
-  
+
+
+class LoopEvent(Event):
+    loop_output: str
+
+
 class MultiStepWorkflow(Workflow):
+    # @step
+    # async def step_one(self, ev: StartEvent) -> ProcessingEvent:
+    #     # Process initial data
+    #     return ProcessingEvent(intermediate_result="Step 1 complete")
+
+    # @step
+    # async def step_two(self, ev: ProcessingEvent) -> StopEvent:
+    #     # Use the intermediate result
+    #     final_result = f"Finished processing: {ev.intermediate_result}"
+    #     return StopEvent(result=final_result)
+
     @step
-    async def step_one(self, ev: StartEvent) -> ProcessingEvent:
-        # Process initial data
-        return ProcessingEvent(intermediate_result="Step 1 complete")
-    
+    async def step_one(self, ev: StartEvent | LoopEvent) -> ProcessingEvent | LoopEvent:
+        if random.randint(0, 1) == 0:
+            print("Bad thing happened")
+            return LoopEvent(loop_output="Back to step one.")
+        else:
+            print("Good thing happened")
+            return ProcessingEvent(intermediate_result="First step complete.")
+
     @step
     async def step_two(self, ev: ProcessingEvent) -> StopEvent:
         # Use the intermediate result
@@ -33,4 +56,5 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
+
     asyncio.run(main())
